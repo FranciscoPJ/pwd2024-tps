0ce51097e407c3ca0b6125d6f603ffe3c68a4791
Rodrigo Alberto Villablanca <138055427+shodrig0@users.noreply.github.com>
2024-08-23 15:11:24 -0300

    action mejorado

--- 3.tp3/1.eje1/vista/action/action.php
+++ 3.tp3/1.eje1/vista/action/action.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $urlDelArchivo = '../resultado.php';
+    $urlDelArchivo = 'action.php';
 
     if (isset($_FILES['archivo'])) {
         require_once '../../control/FileUploadController.php'; // incluye la clase del controlador
@@ -20,5 +20,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // redirigir final
     header("Location: $urlDelArchivo");
+    var_dump($urlDelArchivo);
     exit;
 }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Archivo subido</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Archivo subido</h1>
+        <?php
+        $msj = "";
+        if (isset($_GET['archivo'])) {
+            $nombreArchivo = $_GET['archivo'];
+            //strtoupper
+            $msj = "<p>El archivo '$nombreArchivo' se ha subido correctamente.</p>";
+            $msj .= "<p>Podés acceder al archivo haciendo clic en el siguiente enlace:</p>";
+            $msj .= "<a href='../../control/files/$nombreArchivo'>$nombreArchivo</a>";
+        } elseif (isset($_GET['error'])) {
+            $mensajeError = $_GET['error'];
+            $msj = "<div class='alert alert-danger'>$mensajeError</div>";
+        } else {
+            $msj = "<p>No se ha seleccionado ningún archivo.</p>";
+        }
+        echo $msj;
+        ?>
+        <br>
+        <a href="../subirArchivo.php" class="btn btn-primary">Volver</a>
+    </div>
+</body>
+
+</html>
