7c634f4afb118a1d8a850600dd43687598636aa2
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-08-23 19:44:26 -0300

    Ej1 y Ej2 de Tp3

--- 3.tp3/2.ej2/vista/action/action.php
+++ 3.tp3/2.ej2/vista/action/action.php
@@ -4,10 +4,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $urlDelArchivo = 'action.php';
 
     if (isset($_FILES['archivo'])) {
-        require_once '../../control/FileUploadController.php'; // incluye la clase del controlador
+        require_once '../../control/TextFileUploadController.php'; // Incluye la clase del controlador
 
-        $subirArchivo = new FileUploadController();
-        $nombreArchivoSubido = $subirArchivo->procesarArchivo($_FILES['archivo']);
+        $subirArchivoTexto = new SubirArchivoTextoController();
+        $nombreArchivoSubido = $subirArchivoTexto->procesarArchivo($_FILES['archivo']);
 
         if ($nombreArchivoSubido) {
             $urlDelArchivo .= "?archivo=$nombreArchivoSubido";
@@ -17,8 +17,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $urlDelArchivo .= "?error=No se ha seleccionado ningún archivo.";
     }
-
-    // redirigir final
     header("Location: $urlDelArchivo");
     exit;
 }
@@ -29,21 +27,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
     <meta charset="UTF-8">
-    <title>Archivo subido</title>
+    <title>Resultado Texto Subido</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
     <div class="container mt-5">
-        <h1 class="mb-4">Archivo subido</h1>
+        <h1 class="mb-4">Resultado Texto Subido</h1>
         <?php
         $msj = "";
         if (isset($_GET['archivo'])) {
             $nombreArchivo = $_GET['archivo'];
-            //strtoupper
-            $msj = "<p>El archivo '$nombreArchivo' se ha subido correctamente.</p>";
-            $msj .= "<p>Podés acceder al archivo haciendo clic en el siguiente enlace:</p>";
-            $msj .= "<a href='../../control/files/$nombreArchivo'>$nombreArchivo</a>";
+            $rutaArchivo = '../../control/txtFiles/' . $nombreArchivo;
+
+            if (file_exists($rutaArchivo)) {
+                $contenidoArchivo = file_get_contents($rutaArchivo);
+                $msj = "<p>El archivo '$nombreArchivo' se ha subido correctamente.</p>";
+                $msj .= "<h4>Contenido del Archivo:</h4>";
+                $msj .= "<textarea class='form-control' rows='10'>$contenidoArchivo</textarea>";
+            } else {
+                $msj = "<div class='alert alert-danger'>El archivo '$nombreArchivo' no existe.</div>";
+            }
         } elseif (isset($_GET['error'])) {
             $mensajeError = $_GET['error'];
             $msj = "<div class='alert alert-danger'>$mensajeError</div>";
@@ -53,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo $msj;
         ?>
         <br>
-        <a href="../subirArchivo.php" class="btn btn-primary">Volver</a>
+        <a href="../subirTxt.php" class="btn btn-primary">Volver</a>
     </div>
 </body>
 
