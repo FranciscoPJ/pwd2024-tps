05e0e56673359e395d72f0794b5b12f05a6ec4c0
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-09-12 23:47:08 -0300

    Arregle el Ej4

--- TP4/PDO_ABM/Vista/Action/accionBuscarAuto.php
+++ TP4/PDO_ABM/Vista/Action/accionBuscarAuto.php
@@ -8,88 +8,91 @@ $datos = data_submitted();
 $resp = false;
 $objAbmAuto = new AbmAuto();
 
-if (isset($datos['accion'])){
-    if($datos['accion']=='editar'){
-        if($objAbmAuto->modificacion($datos)){ // modifica
+if (isset($datos['accion'])) {
+    if ($datos['accion'] == 'editar') {
+        if ($objAbmAuto->modificacion($datos)) { // modifica
             $resp = true;
         }
     }
-    if($datos['accion']=='borrar'){
-        if($objAbmAuto->baja($datos)){ // elimina
-            $resp =true;
+    if ($datos['accion'] == 'borrar') {
+        if ($objAbmAuto->baja($datos)) { // elimina
+            $resp = true;
         }
-        
     }
-    if($datos['accion']=='nuevo'){
-        if($objAbmAuto->alta($datos)){ // agrega
-            $resp =true;
+    if ($datos['accion'] == 'nuevo') {
+        if ($objAbmAuto->alta($datos)) { // agrega
+            $resp = true;
         }
-        
     }
-    if($datos['accion']=='buscarPatente'){
-        if($objAbmAuto->buscar($datos)){ // busca
-            $resp =true;
-        } 
+    if ($datos['accion'] == 'buscarPatente') {
+        if ($objAbmAuto->buscar($datos)) { // busca
+            $resp = true;
+        }
     }
-    
-    if($resp){
-        $mensaje = "La accion ".$datos['accion']." se realizo correctamente.";
-    }else {
-        $mensaje = "La accion ".$datos['accion']." no pudo concretarse.";
+
+    if ($resp) {
+        $mensaje = "La accion " . $datos['accion'] . " se realizo correctamente.";
+    } else {
+        $mensaje = "La accion " . $datos['accion'] . " no pudo concretarse.";
     }
 
     $arrayDato = $objAbmAuto->buscar($datos); //array que contine los datos de un auto
-    
+
     $colDatos = $objAbmAuto->solicitarDatos(); //array de preguntas de datos
 }
 ?>
 
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accion Buscar Auto</title>
 </head>
+
 <body>
     <p>
         <?php
-            echo "<h2 align='center'>" . $mensaje . "</h2>";
+        echo "<h2 align='center'>" . $mensaje . "</h2>";
         ?>
     </p>
-    <form align="center">
-        <div>
-            <?php
-                foreach($colDatos as $tipoPregunta => $pregunta){
-                    echo "<label for=" . $tipoPregunta . ">" . $pregunta . "</label><br>";
-                    echo "<input type='text' id='$tipoPregunta' name='$tipoPregunta' style='width: 300px;'>"."<br>";
-                }
-                echo "<br><input type='submit'>";
-            ?>
-        </div>
-    </form>
 
-                <!-- la tabla se tiene que hacer con jquery. si los campos son correctos, se crea la tabla. sino no -->
+    <div>
+        <?php
 
-        <div>
-            <?php
-                if(count($arrayDato) > 0){
+        if (count($arrayDato) > 0) {
 
-                    foreach($arrayDato as $dato){
-                    echo "<table border='1' align='center'>";
-                    echo '<tr><td style="width:100px;">'.$dato->getPatente().'</td>';
+            echo "<table border='1' align='center'>";
+            echo "<tr>";
+            echo "<th>Patente</th>";
+            echo "<th>Marca</th>";
+            echo "<th>Modelo</th>";
+            echo "<th>DNI</th>";
+            echo "</tr>";
 
-                    echo '<td style="width:100px;">'.$dato->getMarca().'</td>';
+            foreach ($arrayDato as $dato) {
 
-                    echo '<td style="width:100px;">'.$dato->getModelo().'</td>';
+                echo '<tr><td style="width:100px;"  align="center">' . $dato->getPatente() . '</td>';
 
-                    echo '<td style="width:100px;">'.$dato->getObjDniDuenio()->getNroDni().'</td>';
+                echo '<td style="width:100px;" align="center">' . $dato->getMarca() . '</td>';
 
-                    echo "</tr></table>";
-                    }
-                }
-                
-            ?>
-        </div>
-    
+                echo '<td style="width:100px;" align="center">' . $dato->getModelo() . '</td>';
+
+                echo '<td style="width:100px;" align="center">' . $dato->getObjDniDuenio()->getNroDni() . '</td>';
+
+                echo "</tr>";
+            }
+
+            echo "</table>";
+
+        } else {
+            echo "<div align='center'>No tiene un auto asociado</div>";
+        }
+
+        ?>
+    </div>
+    <br>
+    <div align="center"><a href="../buscarAuto.php">volver</a></div>
 </body>
+
 </html>
